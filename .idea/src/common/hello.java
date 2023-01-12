ff20b5d376e12b2f7c4661098147d1e2a379b314
杨函泰 <yang010101012020@outlook.com>
2023-01-12 20:38:56 +0800

    test for git

--- .idea/src/common/hello.java
+++ .idea/src/common/hello.java
@@ -1,5 +1,6 @@
 public class hello {
     public static void main(String args[]){
-        System.out.println("hello my friend")
+        System.out.println("hello my friend");
+        System.out.println("hello haowen");
     }
 }
